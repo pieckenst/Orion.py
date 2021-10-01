f49725e7992eb468b6d9b108db2aeb1f7bf3ad22
Benitz Original <benitz@numix.xyz>
2021-10-01 15:51:36 +0800

    Update errors.py

--- discord/errors.py
+++ discord/errors.py
@@ -39,6 +39,8 @@ if TYPE_CHECKING:
     from .interactions import Interaction
 
 __all__ = (
+    'CommandError',
+    'CommandNotFound',
     'NoEntryPointError',
     'ExtensionError',
     'DiscordException',
@@ -63,6 +65,34 @@ class DiscordException(Exception):
     Ideally speaking, this could be caught to handle any exceptions raised from this library.
     """
 
+    pass
+
+class CommandError(DiscordException):
+    r"""The base exception type for all command related errors.
+
+    This inherits from :exc:`discord.DiscordException`.
+
+    This exception and exceptions inherited from it are handled
+    in a special way as they are caught and passed into a special event
+    from :class:`.Bot`\, :func:`.on_command_error`.
+    """
+    def __init__(self, message: Optional[str] = None, *args: Any) -> None:
+        if message is not None:
+            # clean-up @everyone and @here mentions
+            m = message.replace('@everyone', '@\u200beveryone').replace('@here', '@\u200bhere')
+            super().__init__(m, *args)
+        else:
+            super().__init__(*args)
+
+class CommandNotFound(CommandError):
+    """Exception raised when a command is attempted to be invoked
+    but no command under that name is found.
+
+    This is not raised for invalid subcommands, rather just the
+    initial main command that is attempted to be invoked.
+
+    This inherits from :exc:`CommandError`.
+    """
     pass
 
 class ExtensionError(DiscordException):
